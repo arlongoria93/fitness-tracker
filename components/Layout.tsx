5b2c94b8e2b91edd4a4792b84cf1467af760b373
Angelo <arlongoria93@gmail.com>
2022-11-01 12:47:07 -0700

    🎉 init: Tailwind/Prisma

---
+++ components/Layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: Props) => {
+  return (
+    <div>
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
